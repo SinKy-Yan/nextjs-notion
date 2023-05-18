e169f4581cc406232d5dc8ec655271d03f2fea97
源源球球✨ <1340793687@outlook.com>
2023-05-18 14:06:25 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '703564b525d445bfb068cbf223685a93',
+  rootNotionPageId: 'Blog-703564b525d445bfb068cbf223685a93',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: '源源的狗窝',
-  domain: 'https://www.notion.so/703564b525d445bfb068cbf223685a93',
+  domain: 'https://www.notion.so/Blog-703564b525d445bfb068cbf223685a93',
   author: '源源圆球',
 
   // open graph metadata (optional)
